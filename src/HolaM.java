0f07c5172ea20d02d051957bc48586ca53920fe0
Estudiante <Estudiante@10.203.2.143>
2017-02-06 14:40:00 -0500

    es un hola mundo ya acabe

--- src/HolaM.java
+++ src/HolaM.java
@@ -3,6 +3,6 @@ public class HolaM {
     public static void
 		main (String[]arg)
 		{
-                    System.out.println("hola mundo");
+                    System.out.println("Holi Soy NicolDDD y soy enana y consentida");
 		}
 }
